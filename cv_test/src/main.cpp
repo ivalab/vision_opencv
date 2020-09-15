ef820437830faa77179985caeb715513a01fb517
sfeng65 <sfeng65@gatech.edu>
2020-09-15 02:33:53 -0400

    compile with ros opencv and no cuda

--- cv_test/src/main.cpp
+++ cv_test/src/main.cpp
@@ -3,9 +3,9 @@
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/cudaobjdetect.hpp"
-#include "opencv2/cudaimgproc.hpp"
-#include "opencv2/cudawarping.hpp"
+//#include "opencv2/cudaobjdetect.hpp"
+//#include "opencv2/cudaimgproc.hpp"
+//#include "opencv2/cudawarping.hpp"
 
 using namespace cv::cuda;
 
